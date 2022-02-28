dfc8b51ea8d9d6e30ecdd11d7d5e8f0ebbe8465f
hingobway <hingobway@users.noreply.github.com>
2022-02-27 20:13:20 -0500

    security updates and new vue permalink

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,13 @@ const { src, dest, parallel, watch } = require('gulp');
 const babel = require('gulp-babel');
 const prefix = require('gulp-autoprefixer');
 
-const { browserslist } = require('./package.json');
-
 const js = (exports.js = () =>
   src('orig/*.js')
     .pipe(babel({ presets: ['@babel/env'] }))
     .pipe(dest('js/')));
 
 const css = (exports.css = () =>
-  src('orig/*.css')
-    .pipe(
-      prefix({
-        browsers: browserslist
-      })
-    )
-    .pipe(dest('css/')));
+  src('orig/*.css').pipe(prefix()).pipe(dest('css/')));
 
 const prod = (exports.prod = parallel(js, css));
 
